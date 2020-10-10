51600864425aa1e5e4713000891637acab67c07a
RyuseiNomi <kanontonto1010@gmail.com>
2020-10-10 19:08:11 +0900

    Initial Commit

---
+++ RubberDuck-Speaker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RubberDuck-Speaker
+//
+//  Created by 能美龍星 on 2020/10/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RubberDuck-Speaker/RubberDuck_SpeakerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubberDuck_SpeakerApp.swift
+//  RubberDuck-Speaker
+//
+//  Created by 能美龍星 on 2020/10/10.
+//
+
+import SwiftUI
+
+@main
+struct RubberDuck_SpeakerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
